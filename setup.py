1a53478e424970654b44f865c06d32ed7a689eac
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-07 20:00:11 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.txt", "r") as setup:
+    long_description = setup.read()
+
+setuptools.setup(
+    name="mygtu",
+    version="0.1.9",
+    author="g",
+    author_email="k",
+    description="g",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/batatavadaX/testgtu",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
