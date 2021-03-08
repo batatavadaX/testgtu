dd8d69a50aafb6dfb5655585498b3ad1cf6d7d82
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 15:21:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from mygtu import dl, logic
+import asyncio
 
-async def k():
+async def main():
     gf = logic("downloads/k.json")
     db = gf.database()
     urls = gf.gather_url(db)
     await dl.download(urls)
+
+asyncio.run(main())
