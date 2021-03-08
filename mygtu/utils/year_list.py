63d263c1732d2bc89344d6a9c4a811e1f01d776e
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 16:07:38 +0530

    Update year_list.py

--- mygtu/utils/year_list.py
+++ mygtu/utils/year_list.py
@@ -1,1 +1,8 @@
-
+from .time.time import current
+class 
+    @staticmethod
+    def year() -> list:
+        new = []
+        for i in range(1, 4):
+            new.append(int(current()) - i)
+        return new
