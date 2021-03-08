7d594c7aaefb586f8f108283425cbf96dcf7b5c8
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 12:23:44 +0530

    Update dl.py

--- mygtu/dl.py
+++ mygtu/dl.py
@@ -7,33 +7,30 @@ from .utils import time
 from .utils.profile import __logo__, __info__, __version__
 from .utils.constants import PATH
 
-class downloader():
-    @staticmethod
-    async def download(urls):
-        for url in urls:
-            path = PATH + url.split("/uploads/")[1].replace("/", "-")
-            start = datetime.now()
-            async with aiohttp.request("GET", url) as response:
-                read = await response.read()
-                await asyncio.sleep(2)
-            async with aiofiles.open(path, 'wb') as f:
-                await f.write(read)
-            await f.close()
+async def download(urls):
+    for url in urls:
+        path = PATH + url.split("/uploads/")[1].replace("/", "-")
+        start = datetime.now()
+        async with aiohttp.request("GET", url) as response:
+            read = await response.read()
             await asyncio.sleep(2)
-            end = datetime.now()
+        async with aiofiles.open(path, 'wb') as f:
+            await f.write(read)
+        await f.close()
+        await asyncio.sleep(2)
+        end = datetime.now()
 
-        le_n = len(urls)   
-        info = f'''
-        {profile.__logo__}
-        Total Files : {le_n}\n
-        Total Time : [{(le_n * (end - start))}]\n
-        Path : {PATH}\n
-        info : {profile.__info__}\n
-        version: {profile.__version__}\n
-        [{time.current(strf="%m/%d/%Y, %H:%M:%S"}]
-        '''
-        with open(
-            PATH+"info.txt", "w") as w:
-                w.write(info)
-        w.close()
-        print(info)
+le_n = len(urls)   
+info = f'''
+{profile.__logo__}
+Total Files : {le_n}\n
+Total Time : [{(le_n * (end - start))}]\n
+Path : {PATH}\n
+info : {profile.__info__}\n
+version: {profile.__version__}\n
+[{time.current(strf="%m/%d/%Y, %H:%M:%S"}]
+'''
+with open(PATH+"info.txt", "w") as w:
+    w.write(info)
+w.close()
+print(info)
