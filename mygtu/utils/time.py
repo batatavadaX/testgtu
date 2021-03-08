bdcdd7df268a68fee13f0f0d7b9753cfb604d4c6
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 13:17:34 +0530

    Update time.py

--- mygtu/utils/time.py
+++ mygtu/utils/time.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import pytz
 
 class time:
+    def __init__():
+        return "lol"
     @staticmethod 
     def current(zone='Asia/Kolkata', strf='%Y'): 
         return datetime.now( 
