cb52513e5782531fe5a9cef4d8d10ee4fc51f7ba
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 14:12:01 +0530

    Update dl.py

--- mygtu/dl.py
+++ mygtu/dl.py
@@ -8,6 +8,12 @@ from .utils.profile import __logo__, __info__, __version__
 from .utils.constants import PATH
 
 async def download(urls):
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+        print("creating {PATH}")
+    else:
+        print("found path {PATH}")
+
     for url in urls:
         path = PATH + url.split("/uploads/")[1].replace("/", "-")
         start = datetime.now()
