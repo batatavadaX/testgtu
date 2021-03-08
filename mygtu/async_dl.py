462a61946361ad28646bb524806d1ed79c1d1e8b
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 11:40:11 +0530

    Create async_dl.py

---
+++ mygtu/async_dl.py
@@ -0,0 +1,44 @@
+import asyncio
+import aiohttp
+import aiofiles
+import os
+from datetime import datetime 
+from .gtu import logic
+from .profile import *
+from .constants import PATH
+
+gf = logic()
+
+class downloader():
+    def __init__():
+        if not os.path.exists(PATH):
+            os.mkdir(PATH)
+    
+    async def download(urls):
+        for url in urls:
+            path = PATH + url.split("/uploads/")[1].replace("/", "-")
+            start = datetime.now()
+            async with aiohttp.request("GET", url) as response:
+                read = await response.read()
+                await asyncio.sleep(2)
+            async with aiofiles.open(path, 'wb') as f:
+                await f.write(read)
+            await f.close()
+            await asyncio.sleep(2)
+            end = datetime.now()
+
+        le_n = len(urls)   
+        info = f'''
+        {profile.__logo__}
+        Total Files : {le_n}\n
+        Total Time : [{(le_n * (end - start))}]\n
+        Path : {PATH}\n
+        __author__ : {profile.__author__}\n
+        __version__ : {profile.__version__}\n
+        [{gf.current(strf="%m/%d/%Y, %H:%M:%S")}]
+        '''
+        with open(
+            PATH+"info.txt", "w") as w:
+                w.write(info)
+        w.close()
+        print(info)
