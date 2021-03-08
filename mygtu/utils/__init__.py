13c2ace9b37c9e7581331d1cfc54822ea375e4b9
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 16:39:07 +0530

    Update __init__.py

--- mygtu/utils/__init__.py
+++ mygtu/utils/__init__.py
@@ -1,4 +1,4 @@
 from .profile import __logo__, __info__, __version__
-from .constants import PATH, DIR
+from .constants import PATH
 from .time.time import current
 from .year_list.ylist import y_list
