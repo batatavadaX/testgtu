a0513a571f15991c8739cb47820ff0eddbe6c49d
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 18:14:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from mygtu import dl, mad
 import asyncio
 
 async def main():
-    gf = mad("testgtu/mygtu/database/subject_code.json")
+    gf = mad(path="testgtu/mygtu/database/subject_code.json")
     db = gf.database()
     urls = gf.gather_url(db)
     await dl.download(urls)
