c10e36800569f5f7e26afc953f5e086d1f93c651
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-08 11:48:53 +0530

    Update profile.py

--- mygtu/utils/profile.py
+++ mygtu/utils/profile.py
@@ -1,4 +1,12 @@
 __version__ = '0.1.9'
 __info__ = 'following software publish under MIT License [t.me/midnightmadwalk]'
-__logo__ = 'MyGtu'        
+__logo__ = 
+'
+  __  __        ____ _         
+ |  \/  |_   _ / ___| |_ _   _ 
+ | |\/| | | | | |  _| __| | | |
+ | |  | | |_| | |_| | |_| |_| |
+ |_|  |_|\__, |\____|\__|\__,_|
+         |___/                 
+'    
   
