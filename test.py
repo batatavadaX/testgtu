70e40611e9ad7c30cc097c0dbf0e0fe33b6c9395
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-09 18:56:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,27 @@
-from mygtu import dl, logic
+import aiohttp
+import aiofiles
 import asyncio
 
-async def main():
-    gf = logic("testgtu/mygtu/database/subject_code.json")
-    db = gf.database()
-    urls = gf.gather_url(db)
-    await dl.download(urls)
+PATH = 'downloads'
+urls = ['https://www.gtu.ac.in/uploads/S2020/BE/3110006.pdf', 'https://www.gtu.ac.in/uploads/S2020/BE/3110003.pdf']
+async def downloader(url):
+    try:
+        async with aiohttp.request("GET", url) as response:
+            return response.status
+            if response.status==200:
+                path = PATH + url.split("/uploads/")[1].replace("/", "-")
+                async with aiofiles.open(path, 'wb') as mad:
+                    await mad.write(await response.read())
 
-asyncio.run(main())
+    except Exception:
+        print("URL Broken:", url)
+
+async def main(loop, urls):
+    task = [downloader(url) for url in urls]
+    await asyncio.gather(*task)
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop) 
+
+def download(urls):
+    loop.run_until_complete(main(loop, urls))
