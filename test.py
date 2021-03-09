48b53204beb0c59531baf0594369229dc1b9ae7d
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-09 08:43:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from mygtu import dl, gf
 import asyncio
+import uvloop
 
 async def main():
     db = gf.database()
     urls = gf.gather_url(db)
     await dl.download(urls)
 
+uvloop.install()
 asyncio.run(main())
