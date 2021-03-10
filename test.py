ebffd903bc9286d302218ecf7a4e08c12aefcfba
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-10 08:33:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,40 @@
-from mygtu import dl, mad
+import aiohttp
+import aiofiles
 import asyncio
+from datetime import datetime
+from mygtu import PATH, gf
+from tqdm import tqdm
 
-async def main():
-    gf = mad(path="testgtu/mygtu/database/subject_code.json")
-    db = gf.database()
-    urls = gf.gather_url(db)
-    await dl.download(urls)
+urls = gf.gather_url()
 
-asyncio.run(main())
+async def lol(url):
+  async with aiohttp.request("GET", url) as res:
+    return int(res.headers.get('content-length', 0))
+
+async def downloader(url):
+    async with aiohttp.request("GET", url) as r:
+        if "uploads" in url:
+            path = PATH + url.split("/uploads/")[1].replace("/", "-")
+        else:
+            path = url.split("//")[1].replace("/", "-")
+        async with aiofiles.open(path, 'wb') as mad:
+          await mad.write(await r.read())
+
+async def main(urls):
+    
+    for url in urls:
+        le_n = len(urls)
+        start = datetime.now()
+        await downloader(url),
+        end = datetime.now()
+        fin = (end - start)
+        for i in tqdm(range(100)):
+            pass
+        break
+
+    print(f"downloaded in [{(le_n * fin)}]")
+   
+async def download(urls):
+    await main((urls))
+
+asyncio.run(download(urls))
